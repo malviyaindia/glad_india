915b13a5b479819f9700de4c6267cfed81cf0f45
Atul Malviya <75677395+malviyaindia@users.noreply.github.com>
2021-01-31 12:01:22 +0530

    Add files via upload

--- Biodata-resume-maker.js
+++ Biodata-resume-maker.js
@@ -11,14 +11,19 @@ function data(){
  let mob=  document.getElementById("mob").value;
  
  let mail=  document.getElementById("mail").value;
- let height=  document.getElementById("height").value;
- let weight=  document.getElementById("weight").value;
+ let address=  document.getElementById("height").value;
+ let marital=  document.getElementById("weight").value;
+  let nationality=  document.getElementById("weight2").value;
 let extra1 = document.getElementsByClassName('in')[0].value;
 let extra2 = document.getElementsByClassName('in')[1].value;
+let extra3 = document.getElementsByClassName('in')[2].value;
+let extra4 = document.getElementsByClassName('in')[3].value;
 
- let x = '<b>Frist Name</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+fname+'<i>'+'<br>'+'<b>Last Name</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+lname+'<i>'+'<br>'+'<b>Date Of Birth </b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+date+'<i>'+'<br>'+'<b>Education </b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+edu+'<i>'+'<br>'+'<b> Profession </b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+pro+'<i>'+'<br>' + '<b>Gender </b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+gen+'<i>'+'<br>'+'<b>Mobile Number </b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+mob+'<i>'+'<br>'+' <b>Email</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+mail+'<i>'+'<br>'+'<b>Height </b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+height+'<i>'+'<br>'+'<b>Weight</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+weight+'<br>'+'<b>'+extra1+'</b>'+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+extra2+'<i>';
+let x = '<b>Frist Name :</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+fname+'<i>'+'<br>'+'<b>Father Name :</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+lname+'<i>'+'<br>'+'<b>Date Of Birth :</b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+date+'<i>'+'<br>'+'<b>Education :</b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+edu+'<i>'+'<br>'+'<b> Profession :</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+pro+'<i>'+'<br>' + '<b>Gender :</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+gen+'<i>'+'<br>'+'<b>Mobile Number :</b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+mob+'<i>'+'<br>'+' <b>Email :</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+mail+'<i>'+'<br>'+'<b>Address :</b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+address+'<i>'+'<br>'+'<b>Marital Status :</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+marital+'<i>'+'<br>'+'<b>Nationality :</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+nationality+'<br>'+'<b>'+extra1+'</b>'+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+extra2+'<i>'+'<br>'+'<b>'+extra3+'</b>'+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+'<i>'+extra4+'<i>';
+ 
  document.getElementById("demo").innerHTML= x;
 
+document.getElementsByClassName("fa-print")[0].style.display= "block";
 };
 let loadFile = function(event) {
 	let image = document.getElementById('output');
@@ -44,3 +49,4 @@ function openNav() {
   document.getElementById("mySidenav").style.width = "100%"}
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0px"}
+  
