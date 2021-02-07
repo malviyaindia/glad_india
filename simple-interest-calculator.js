026015f62c3f43b52d23b4fc61e093b46a240ab9
Atul Malviya <75677395+malviyaindia@users.noreply.github.com>
2021-02-07 20:09:45 +0530

    Add files via upload

--- simple-interest-calculator.js
+++ simple-interest-calculator.js
@@ -91,3 +91,17 @@
     }
     
 }
+
+function openNav(){
+  
+  document.getElementById("mySidenav").style.width= "99.9%";
+ 
+  
+}
+function closeNav(){
+  
+  document.getElementById("mySidenav").style= "width: 0%";
+  
+}
+
+
